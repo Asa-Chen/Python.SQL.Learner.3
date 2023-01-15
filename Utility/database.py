918c78c8d116904535923739433b8ec2be6111b1
Asa-Chen <ac581218@ohio.edu>
2023-01-15 15:05:06 -0500

    PosgtreSQL conversion

--- Utility/database.py
+++ Utility/database.py
@@ -1,75 +1,91 @@
-import sqlite3
+import os
+import psycopg2
+from dotenv import load_dotenv
+load_dotenv() #When left empty, it will try and load the .env in the main directory, meaning you can use DATABASE_URL f
+#from .env
 CREATE_MOVIE_TABLE = """CREATE TABLE IF NOT EXISTS movies (
-    id INTEGER PRIMARY KEY,
-    title,
-    release_date)"""
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    release_date DATE);"""
 CREATE_USER_TABLE = """CREATE TABLE IF NOT EXISTS users (
     username TEXT PRIMARY KEY
-    )"""
+    );"""
 CREATE_WATCHED_TABLE = """CREATE TABLE IF NOT EXISTS watched (
-    user_name,
+    user_name TEXT,
     movie_id INTEGER,
     FOREIGN KEY(user_name) REFERENCES users(username),
     FOREIGN KEY(movie_id) REFERENCES movies(id)
-    )"""
-INSERT_MOVIES = "INSERT INTO movies (title, release_date) VALUES (?, ?)"
-INSERT_WATCHED = "INSERT INTO watched (user_name, movie_id) VALUES (?, ?)"
-INSERT_USER = "INSERT INTO users (username) VALUES (?)"
-DELETE_MOVIES = "DELETE FROM movies WHERE title=?"
-SELECT_ALL_MOVIES = "SELECT * FROM movies"
+    );"""
+INSERT_MOVIES = "INSERT INTO movies (title, release_date) VALUES (%s, %s);"
+INSERT_WATCHED = "INSERT INTO watched (user_name, movie_id) VALUES (%s, %s);"
+INSERT_USER = "INSERT INTO users (username) VALUES (%s);"
+DELETE_MOVIES = "DELETE FROM movies WHERE title=%s;"
+SELECT_ALL_MOVIES = "SELECT * FROM movies;"
 SELECT_WATCHED_NEW = """SELECT movies.*
     FROM movies
     JOIN watched ON watched.movie_id = movies.id
     JOIN users ON users.username = watched.user_name
-    WHERE users.username=?"""
-SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_date > ?"
-SEARCH_MOVIES = "SELECT * FROM movies WHERE title LIKE ?"
+    WHERE users.username=%s;"""
+SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_date > %s;"
+SEARCH_MOVIES = "SELECT * FROM movies WHERE title LIKE %s;"
 
-connection = sqlite3.connect('data.db')
-
-def create_table():
+connection = psycopg2.connect(os.environ["DATABASE_URL"]) #os is a standard package, environ is a dictionary
+#psycopg2 also requires cursors
+def create_table(): #PostgreSQL requires a cursor
     with connection:
-        connection.execute(CREATE_MOVIE_TABLE)
-        connection.execute(CREATE_WATCHED_TABLE)
-        connection.execute(CREATE_USER_TABLE)
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_MOVIE_TABLE)
+            cursor.execute(CREATE_USER_TABLE)
+            cursor.execute(CREATE_WATCHED_TABLE)
+
 
 def add_movie(title, release_date):
     with connection:
-        connection.execute(INSERT_MOVIES, (title, release_date,))
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_MOVIES, (title, release_date,))
 
 def add_user(user):
     with connection:
-        connection.execute(INSERT_USER, (user,))
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_USER, (user,))
 
 def delete_movie(title):
     with connection:
-        connection.execute(DELETE_MOVIES, (title,))
+        with connection.cursor() as cursor:
+            cursor.execute(DELETE_MOVIES, (title,))
 
 def select_movies(): #returns a list of tuples
     with connection:
-        return connection.execute(SELECT_ALL_MOVIES).fetchall()
+        with connection.cursor() as cursor:
+            return cursor.execute(SELECT_ALL_MOVIES).fetchall()
 
 def select_upcoming(date):
     with connection:
-        return connection.execute(SELECT_UPCOMING_MOVIES, (date,)).fetchall()
+        with connection.cursor() as cursor:
+            return cursor.execute(SELECT_UPCOMING_MOVIES, (date,)).fetchall()
 
 def select_watched(user): #returns a list of tuples
     with connection:
-        return connection.execute(SELECT_WATCHED_NEW, (user,)).fetchall()
+        with connection.cursor() as cursor:
+            return cursor.execute(SELECT_WATCHED_NEW, (user,)).fetchall()
 
 def search_movies(target):
     with connection:
-        return connection.execute(SEARCH_MOVIES, (f"%{target}%", )).fetchall()
+        with connection.cursor() as cursor:
+            return cursor.execute(SEARCH_MOVIES, (f"%{target}%", )).fetchall()
 
 def mark_movie(user, movie_id):
     with connection:
-        connection.execute(INSERT_WATCHED, (user, movie_id,))
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_WATCHED, (user, movie_id,))
 
 def check_movie_presense(movie_id):
     with connection: #bool of a list returned by a cursor.fetchall() from connection
-        return bool(connection.execute("SELECT * FROM movies WHERE id=?", (movie_id,)).fetchall())
+        with connection.cursor() as cursor:
+            return bool(cursor.execute("SELECT * FROM movies WHERE id=%s;", (movie_id,)).fetchall())
 
 def check_user_presense(user_id):
     with connection:
-        return bool(connection.execute("SELECT * FROM users WHERE username=?", (user_id,)).fetchall())
+        with connection.cursor() as cursor:
+            return bool(cursor.execute("SELECT * FROM users WHERE username=%s;", (user_id,)).fetchall())
 
